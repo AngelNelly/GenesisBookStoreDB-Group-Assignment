6f1009664cfa4fc2adff194cecd52ed7b32ebb3e
AngelNelly <talktoangelnelly@gmail.com>
2025-04-12 18:20:21 +0100

    I created a Table for authors and books and added some datas into it using 'Insert Into'

--- genesisbookstoredb.sql
+++ genesisbookstoredb.sql
@@ -2,11 +2,32 @@ CREATE DATABASE GenesisBookStorDB;
 
 USE GenesisBookStorDB;
 
-CREATE TABLE Books (
-    BookID INT PRIMARY KEY AUTO_INCREMENT,
-    Title VARCHAR(255) NOT NULL,
-    Author VARCHAR(255) NOT NULL,
-    Genre VARCHAR(100),
-    Price DECIMAL(10, 2) NOT NULL,
-    Stock INT NOT NULL DEFAULT 0
+CREATE TABLE Authors (
+    AuthorID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    Bio TEXT
 );
+
+CREATE TABLE Books (
+    BookID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(150),
+    Genre VARCHAR(50),
+    Price DECIMAL(6,2),
+    AuthorID INT,
+    FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID)
+);
+
+
+INSERT INTO Authors (Name, Bio) VALUES
+('J.K. Rowling', 'Author of Harry Potter'),
+('George Orwell', 'Author of 1984');
+
+
+INSERT INTO Books (Title, Genre, Price, AuthorID) VALUES
+('Harry Potter', 'Fantasy', 29.99, 1),
+('1984', 'Dystopian', 19.99, 2);
+
+
+
+CREATE USER 'salesperson'@'localhost' IDENTIFIED BY 'salepass';
+GRANT SELECT, INSERT ON GenesisBookStorDB.* TO 'salesperson'@'localhost';
